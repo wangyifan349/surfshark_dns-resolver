d1905b3a7066179d4e1a83d60d384f214fe9a88b
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-05-28 00:39:45 +0000

    Update dns查询写入hosts.py

--- dns查询写入hosts.py
+++ dns查询写入hosts.py
@@ -1,6 +1,9 @@
 import requests
 import os
-
+import platform
+import concurrent.futures
+print("请确保hosts可以被修改，拥有修改的权限")
+print("这部分代码利用hosts修改了DNS污染问题，防止DNS污染。")
 def dns_query(domain):
     url = "https://1.1.1.1/dns-query"
     params = {
@@ -22,21 +25,35 @@ def dns_query(domain):
                 return ip_address
     return None
 
+def get_hosts_path():#根据系统类型，返回hosts的位置。
+    system = platform.system()
+    if system == "Windows":
+        return r"C:\Windows\System32\drivers\etc\hosts"
+    elif system == "Linux" or system == "Darwin":
+        return "/etc/hosts"
+    else:
+        raise Exception("Unsupported operating system")
+
 def write_to_hosts_file(domain, ip_address):
-    hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+    hosts_path = get_hosts_path()
 
     with open(hosts_path, "a") as hosts_file:
         hosts_file.write(f"{ip_address}\t{domain}\n")
 
 def change_hosts_file_permission():
-    hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-    os.chmod(hosts_path, 0o777)  # 修改hosts文件的权限
+    hosts_path = get_hosts_path()
+    os.chmod(hosts_path, 0o777)  # 修改hosts文件的权限，Windows下请确保那个文件可以被修改哦。
+
+def resolve_domain(domain):
+    ip_address = dns_query(domain)
+    if ip_address:
+        write_to_hosts_file(domain, ip_address)
+        print(f"成功解析 {domain} 为 {ip_address}")
+    else:
+        print(f"无法解析 {domain}")
 
 # 示例使用
 domains = [
-    "baiduapi2.bestmyself.live",
-    "cloundcone.bestmyself.live",
-    "yyds.bestmyself.live",
     "jp-tok.prod.surfshark.com",
     "hk-hkg.prod.surfshark.com",
     "jp-tok.prod.surfshark.com",
@@ -54,16 +71,29 @@ domains = [
     "nl-ams.prod.surfshark.com",
     "se-sto.prod.surfshark.com",
     "ch-zur.prod.surfshark.com",
-    "no-osl.prod.surfshark.com"
+    "no-osl.prod.surfshark.com",
+    "kr-seo.prod.surfshark.com",
+    "my-kul.prod.surfshark.com",
+    "kh-pnh.prod.surfshark.com",
+    "my.surfshark.com"
 ]
 
+
+unique_domains = set(domains)
+unique_domains = list(unique_domains)
+domains=unique_domains    #去重
 change_hosts_file_permission()  # 修改hosts文件的权限
 
-for domain in domains:
-    ip_address = dns_query(domain)
-    if ip_address:
-        write_to_hosts_file(domain, ip_address)
-        print(f"成功解析 {domain} 为 {ip_address}")
-    else:
-        print(f"无法解析 {domain}")
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = []  # 存储任务结果的列表
+    for domain in domains:
+        task = executor.submit(resolve_domain, domain)  # 提交任务给线程池执行
+        results.append(task)  # 将任务对象添加到结果列表中
 
+for result in concurrent.futures.as_completed(results):# 获取查询结果
+    try:
+        result.result()  # 等待任务完成并获取结果
+    except Exception as e:
+        print(f"查询出错：{str(e)}")
+
+print("全部查询完毕，并写入hosts文件中了")
